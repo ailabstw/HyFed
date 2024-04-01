8c57abdcb25203ba3ffab2b39ec157899f9e8eb2
jianhung.wen <jianhung.wen@ailabs.tw>
2024-04-01 02:35:02 +0000

    Make api similar to numpy

--- test/test_rand.py
+++ test/test_rand.py
@@ -6,22 +6,23 @@ import numpy as np
 class StrongRandomTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.n = 100
+        self.size = (3, 4, 5)
 
     def tearDown(self):
         self.n = None
 
-    def test_randint64(self):
-        result = hyfed.rand.randint64(self.n)
-        self.assertEqual((self.n, ), result.shape)
+    def test_randint(self):
+        low, high = 20, 80
+        result = hyfed.randint(low, high, size=self.size)
+        self.assertEqual(self.size, result.shape)
         self.assertEqual(np.int64, result.dtype)
 
     def test_rand(self):
-        result = hyfed.rand.rand(0., 1., self.n, dtype=np.float64)
-        self.assertEqual((self.n, ), result.shape)
+        result = hyfed.rand(*self.size)
+        self.assertEqual(self.size, result.shape)
         self.assertEqual(np.float64, result.dtype)
 
     def test_randn(self):
-        result = hyfed.rand.randn(0., 1., self.n, dtype=np.float64)
-        self.assertEqual((self.n, ), result.shape)
+        result = hyfed.randn(*self.size)
+        self.assertEqual(self.size, result.shape)
         self.assertEqual(np.float64, result.dtype)
